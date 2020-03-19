1cbc420f1ed0c7bd1d361d3750fb957475f5fce9
DdraigGoch <vincentowens@yahoo.com>
2020-03-19 14:09:09 +0000

    Updated March 2020 due to temporary closure

--- tabHome.php
+++ tabHome.php
@@ -22,6 +22,8 @@ and where to find us.</p>
 <p class="style1">To find who's who please visit <a href="#" id="tabHomeAbout">About Us</a></p>
 
 <p class="style1">If you wish to get in contact with the 
-Mess please do not hesitate to <a href="#" id="tabHomeCall">call</a>.</p>
-<!--<p>The 2019 Annual General Meeting will be held on Friday 31st May.</p> <div  id="usmkicker"> <p>The United Services Mess will hold its <strong>104th</strong> Annual Dinner on Friday 8th November 2019. </div>  <br></br>--> 
+Mess please <!-- do not hesitate to --> <a href="#" id="tabHomeCall">email</a>.</p>
+<!-- <p>The 2019 Annual General Meeting will be held on Friday 31st May.</p> --> <div  id="usmkicker"> <p>Due to the Coronavirus outbreak, the United Services Mess is currently closed until until April 30th and therefore all functions during that time have been postponed.</p>
+<p>This decision has been made in the best interest of members and their guests and will be reviewed during this period.</p>
+<p>The Mess eMails will be monitored every day, therefore any communication can be made by eMail.</p> </div>  <br></br><!-- --> 
 </div>
